37007ea6c2e25e5aa0b2c50e268244e910fe1cfd
huangtao117 <huangtao117@gmail.com>
2019-08-23 14:48:44 +0800

    init

--- README.md
+++ README.md
@@ -3,7 +3,16 @@
 ### 说明
 - 必须使用Visual Studio 2015编译（其他版本需要自行编译依赖库）。
 - cmake后用vs2015生成可执行文件后将dependencies\bin目录下的文件拷贝的可执行文件目录。
-- 除了CEGUI提供的依赖库以外还需要freegult(已编译好)。
+- 除了CEGUI提供的依赖库以外还需要GLFW(已编译好)。
+
+### 依赖列表
+* CEGUIBase
+* CEGUIOpenGLRenderer
+* CEGUICoreWindowRendererSet
+* default CEGUI xml parser (and dependencies)
+* GLFW3
+* OpengGL
+* glm headers (as part of CEGUIBase)
 
 ### 例子列表
 - helloworld：CEGUI的helloworld。
--- helloworld/main.cpp
+++ helloworld/main.cpp
@@ -1,125 +1,178 @@
 // http://cegui.org.uk/wiki/Using_CEGUI_with_GLUT
 
+#include <iostream>
 #include <CEGUI/CEGUI.h>
 #include <CEGUI/RendererModules/OpenGL/GLRenderer.h>
-#include <GL/freeglut.h>
 #include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
+using namespace CEGUI;
 
-int window_id;
-bool keep_running = true;
+static GLFWwindow *window;
 
-void render(void);
-void keyFunc(unsigned char key, int x, int y);
-void resource_group();
-void load_data_files();
-void create_window();
+const int win_w = 640;
+const int win_h = 480;
 
-int main(int argc, char **argv)
+MouseButton toCEGUIButton(int button)
 {
-    // 创建OpenGL窗口
-    glutInit(&argc, argv);
-    glutInitWindowSize(640, 480);
-    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    window_id = glutCreateWindow("Hello World");
+    switch (button) {
+    case GLFW_MOUSE_BUTTON_LEFT:
+        return LeftButton;
+    case GLFW_MOUSE_BUTTON_MIDDLE:
+        return MiddleButton;
+    case GLFW_MOUSE_BUTTON_RIGHT:
+        return RightButton;
+    default:
+        return MouseButtonCount;
+    }
+}
 
-    // 回调函数
-    glutKeyboardFunc(keyFunc);
-    glutDisplayFunc(render);
+void charCallback(GLFWwindow *window, unsigned int char_pressed)
+{
+    System::getSingleton().getDefaultGUIContext().injectChar(char_pressed);
+}
 
-    GLenum err = glewInit();
+void cursorPosCallback(GLFWwindow *window, double x, double y)
+{
+    System::getSingleton().getDefaultGUIContext().injectMousePosition(x, y);
+}
 
-    // 初始化CEGUI
-    CEGUI::OpenGLRenderer &myRenderer =
+static void keyCallback(GLFWwindow *window, int key, int scan, int action, int mod) 
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
+
+void mouseButtonCallback(GLFWwindow *window, int button, int state, int mod)
+{
+    if (state == GLFW_PRESS) {
+        System::getSingleton().getDefaultGUIContext().injectMouseButtonDown(
+            toCEGUIButton(button));
+    } else {
+        System::getSingleton().getDefaultGUIContext().injectMouseButtonUp(
+            toCEGUIButton(button));
+    }
+}
+
+void mouseWheelCallback(GLFWwindow *window, double x, double y)
+{
+    if (y < 0.f) {
+        System::getSingleton().getDefaultGUIContext().injectMouseWheelChange(
+            -1.f);
+    } else {
+        System::getSingleton().getDefaultGUIContext().injectMouseWheelChange(
+            +1.f);
+    }
+}
+
+void windowResizedCallback(GLFWwindow *window, int width, int height)
+{
+    System::getSingleton().notifyDisplaySizeChanged(
+        Sizef(static_cast<float>(width), static_cast<float>(height)));
+    glViewport(0, 0, width, height);
+}
+
+void errorCallback(int error, const char *message)
+{
+    Logger::getSingleton().logEvent(message, Errors);
+}
+
+void setupCallbacks()
+{
+    // 输入回调
+    glfwSetCharCallback(window, charCallback);
+    glfwSetCursorPosCallback(window, cursorPosCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetScrollCallback(window, mouseWheelCallback);
+
+    // 窗口回调
+    glfwSetWindowSizeCallback(window, windowResizedCallback);
+
+    // 错误回调
+    glfwSetErrorCallback(errorCallback);
+}
+
+void initGLFW()
+{
+    // glfw库初始化
+    if (!glfwInit()) {
+        std::cerr << "glfw could not be initialized!" << std::endl;
+        exit(1);
+    }
+
+    // 创建窗口和OpenGL设备上下文
+    window = glfwCreateWindow(win_w, win_h, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cerr << "Could not create glfw window!" << std::endl;
+        glfwTerminate();
+        exit(1);
+    }
+
+    // 窗口设备指定为当前
+    glfwMakeContextCurrent(window);
+
+    // 隐藏系统鼠标
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+
+    // 禁用VSYNC
+    glfwSwapInterval(0);
+
+    // 清除错误信息
+    glGetError();
+}
+
+void initCEGUI()
+{
+    // 创建渲染器并打开附加状态
+    OpenGLRenderer &myRenderer =
         CEGUI::OpenGLRenderer::bootstrapSystem();
-    // 或使用下面两句
-    // CEGUI::OpenGLRenderer& myRenderer = CEGUI::OpenGLRenderer::create();
-    // CEGUI::System::create(myRenderer);
+    myRenderer.enableExtraStateSettings(true);
 
-    // 资源设定
-    resource_group();
-    load_data_files();
-    create_window();
-
-    // 消息循环
-    while (keep_running) {
-        glutMainLoopEvent();
-        render();
-
-        // 渲染GUI
-        CEGUI::System::getSingleton().renderAllGUIContexts();
-
-        glutSwapBuffers();
-    }
-
-    // 清理
-    CEGUI::System::destroy();
-    CEGUI::OpenGLRenderer::destroy(myRenderer);
-    glutDestroyWindow(window_id);
-
-    return 0;
-}
-
-// 渲染函数
-void render()
-{
-    glLoadIdentity();
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glutWireTeapot(0.5);
-}
-
-// 按键响应
-void keyFunc(unsigned char key, int x, int y)
-{
-    switch (key) {
-    case 113: // 'q'
-    case 81:  // 'Q'
-    case 27:  // 'ESC'
-        keep_running = false;
-        break;
-    }
-}
-
-void resource_group()
-{
     // 为默认资源组初始化需要的目录
-    CEGUI::DefaultResourceProvider *rp =
-        static_cast<CEGUI::DefaultResourceProvider *>(
-            CEGUI::System::getSingleton().getResourceProvider());
-
-    rp->setResourceGroupDirectory("schemes", "../../../datafiles/schemes/");
-    rp->setResourceGroupDirectory("imagesets", "../../../datafiles/imagesets/");
-    rp->setResourceGroupDirectory("fonts", "../../../datafiles/fonts/");
-    rp->setResourceGroupDirectory("layouts", "../../../datafiles/layouts/");
+    DefaultResourceProvider *rp = static_cast<DefaultResourceProvider *>(
+        System::getSingleton().getResourceProvider());
+    rp->setResourceGroupDirectory("schemes", "datafiles/schemes/");
+    rp->setResourceGroupDirectory("imagesets", "datafiles/imagesets/");
+    rp->setResourceGroupDirectory("fonts", "datafiles/fonts/");
+    rp->setResourceGroupDirectory("layouts", "datafiles/layouts/");
     rp->setResourceGroupDirectory("looknfeels",
-                                  "../../../datafiles/looknfeel/");
+                                  "datafiles/looknfeel/");
     rp->setResourceGroupDirectory("lua_scripts",
-                                  "../../../datafiles/lua_scripts/");
+                                  "datafiles/lua_scripts/");
+    rp->setResourceGroupDirectory("schemas", "datafiles/xml_schemas/");
 
     // 使用默认资源组
-    CEGUI::ImageManager::setImagesetDefaultResourceGroup("imagesets");
-    CEGUI::Font::setDefaultResourceGroup("fonts");
-    CEGUI::Scheme::setDefaultResourceGroup("schemes");
-    CEGUI::WidgetLookManager::setDefaultResourceGroup("looknfeels");
-    CEGUI::WindowManager::setDefaultResourceGroup("layouts");
-    CEGUI::ScriptModule::setDefaultResourceGroup("lua_scripts");
+    ImageManager::setImagesetDefaultResourceGroup("imagesets");
+    Font::setDefaultResourceGroup("fonts");
+    Scheme::setDefaultResourceGroup("schemes");
+    WidgetLookManager::setDefaultResourceGroup("looknfeels");
+    WindowManager::setDefaultResourceGroup("layouts");
+    ScriptModule::setDefaultResourceGroup("lua_scripts");
+
+    XMLParser *parse = System::getSingleton().getXMLParser();
+    if (parse->isPropertyPresent("SchemaDefaultResourceGroup"))
+        parse->setProperty("SchemaDefaultResourceGroup", "schemas");
+
+    // 加载TaharezLook清单和字体
+    SchemeManager::getSingleton().createFromFile("TaharezLook.scheme",
+                                                 "schemes");
+    FontManager::getSingleton().createFromFile("DejaVuSans-10.font");
+
+    // 设置默认字体、鼠标和提示
+    System::getSingleton().getDefaultGUIContext().setDefaultFont(
+        "DejaVuSans-10");
+    System::getSingleton()
+        .getDefaultGUIContext()
+        .getMouseCursor()
+        .setDefaultImage("TaharezLook/MouseArrow");
+    System::getSingleton().getDefaultGUIContext().setDefaultTooltipType(
+        "TaharezLook/Tooltip");
 }
 
-// 加载资源
-void load_data_files()
+void initWindows()
 {
-    CEGUI::SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
-    CEGUI::FontManager::getSingleton().createFromFile("DejaVuSans-10.font");
-
-    CEGUI::System::getSingleton().getDefaultGUIContext().setDefaultFont("DejaVuSans-10");
-    CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
-    CEGUI::System::getSingleton().getDefaultGUIContext().setDefaultTooltipType("TaharezLook/Tooltip");
-}
-
-void create_window()
-{
-    using namespace CEGUI;
     WindowManager &wmgr = WindowManager::getSingleton();
 
     // root是铺满整个窗口的，它是一个空窗口(全透明)
@@ -135,12 +188,81 @@ void create_window()
     // a - 相对量 b - 绝对量
     // 本例子中窗口宽度为640
     // UVector2(UDim(0.0f, 10.0f), UDim(0.0f, 0.0f)) - 窗口左上角位于(10,0)
-    // UVector2(UDim(0.5f, 10.0f), UDim(0.0f, 0.0f)) - 窗口左上角位于(640*0.5+10,0)
+    // UVector2(UDim(0.5f, 10.0f), UDim(0.0f, 0.0f)) -
+    // 窗口左上角位于(640*0.5+10,0)
     fWnd->setPosition(UVector2(UDim(0.25f, 0.0f), UDim(0.25f, 0.0f)));
     // 窗口大小
     fWnd->setSize(USize(UDim(0.5f, 0.0f), UDim(0.5f, 0.0f)));
-    //fWnd->setMaxSize(USize(cegui_reldim(1.0f), cegui_reldim(1.0f)));
+    // fWnd->setMaxSize(USize(cegui_reldim(1.0f), cegui_reldim(1.0f)));
 
     // 标题
     fWnd->setText("Hello World!");
 }
+
+#ifdef _MSC_VER
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow)
+#else
+int main(int argc, char *argv[])
+#endif
+{
+    // 初始化glfw
+    initGLFW();
+
+    // 初始化cegui
+    initCEGUI();
+
+    // 设置glfw回调
+    setupCallbacks();
+
+    // 将窗口尺寸通知系统
+    System::getSingleton().notifyDisplaySizeChanged(Sizef(win_w, win_h));
+    glViewport(0, 0, win_w, win_h);
+
+    // 初始化窗口和布局
+    initWindows();
+
+    // 设定清理颜色
+    glClearColor(0, 0, 0, 255);
+
+    double time = glfwGetTime();
+
+    OpenGLRenderer* renderer = static_cast<OpenGLRenderer*>(System::getSingleton().getRenderer());
+    // 消息循环
+    while (glfwWindowShouldClose(window) == GL_FALSE) {
+        // 清除屏幕
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // 注入定时器
+        //const float newtime = glfwGetTime();
+        //const float time_elapsed = newtime - time;
+        //System::getSingleton().injectTimePulse(time_elapsed);
+        //System::getSingleton().getDefaultGUIContext().injectTimePulse(time_elapsed);
+        //time = newtime;
+
+        // 渲染GUI
+        renderer->beginRendering();
+        System::getSingleton().renderAllGUIContexts();
+        renderer->endRendering();
+
+        // 交换缓冲
+        glfwSwapBuffers(window);
+
+        // 处理事件
+        glfwPollEvents();
+    }
+
+    // 清理
+    System::destroy();
+    OpenGLRenderer::destroy(*renderer);
+    renderer = nullptr;
+
+    // 销毁窗口
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
+
+
+
